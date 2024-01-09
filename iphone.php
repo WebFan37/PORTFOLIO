fb581e5d51fc623fbb274874cefc0089ea6d41f2
WebFan37 <jeanneel2000@gmail.com>
2024-01-09 17:12:34 -0500

    iPhone photo portfolio adding all picture and arrange into gallery style. Add z-index for back button.

--- iphone.php
+++ iphone.php
@@ -7,4 +7,199 @@ include("Common/header.php");
 <?php include("Common/category-menu.php"); ?>
 <?php include("Common/button-back.php"); ?>
 
+<section class="gallery">
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/1.JPG" alt="Moutains around Banff tunnel mountain">
+        <div class="description">
+            <p>Mountains around Banff tunnel mountain</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/2.JPG" alt="Buffet on a cruise!">
+        <div class="description">
+            <p>Buffet on a cruise!</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/3.JPG" alt="Strawberry dessert">
+        <div class="description">
+            <p>Strawberry dessert</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/4.jpg" alt="Cruise on calm sea">
+        <div class="description">
+            <p>Cruise on calm sesa</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/5.JPG" alt="Smokey view of Banff">
+        <div class="description">
+            <p>Smokey view of Banff</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/6.JPG" alt="Sky view with huge mountain ahead">
+        <div class="description">
+            <p>The highway to heaven scenery</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/7.JPEG" alt="Rock and cliff on the coast of the sea">
+        <div class="description">
+            <p>Juan de Fuca National Park BC</p>
+        </div> 
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/8.JPG" alt="Mayan Ruins close shot">
+        <div class="description">
+            <p>Mayan Ruins</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/9.JPG" alt="Mayan Ruins sit beside a shallow cliff to the caribbean sea">
+        <div class="description">
+            <p>Mayan Ruins 2</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/10.JPG" alt="Sunset view from Mont-Royal mountain">
+        <div class="description">
+            <p>Sunset in Montreal</p> 
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/11.JPG" alt="Epic Lake in Banff with snow on mountain">
+        <div class="description">
+            <p>Morraine Lake Banff</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/12.JPG" alt="A orange red background color with the sun setting down">
+        <div class="description">
+            <p>The New Orleans sunset</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/13.JPG" alt="A town and moutains view from high above">
+        <div class="description">
+            <p>Osoyoos</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/14.JPG" alt="A street view from a bus stop">
+        <div class="description">
+            <p>Montreal street view</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/15.JPG" alt="Sea flowing blurr effect">
+        <div class="description">
+            <p>Sea long exposure shot</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/16.jpeg" alt="A statue in the center of a dried fountain with blue sky">
+        <div class="description">
+            <p>Statue Maisonneuve Market</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/17.jpg" alt="Clean stream flowing down a busy leafs area">
+        <div class="description">
+            <p>The flowing of autumn water</p>
+        </div> 
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/18.JPG" alt="A bike lane beside a canal in sunny summer day">
+        <div class="description">
+            <p>Beauty summer</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/19.JPG" alt="A blurry sunrise view with front of the boat focused">
+        <div class="description">
+            <p>The Dream Sunrise</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/20.jpeg" alt="Wall like texture">
+        <div class="description">
+            <p>Texture 1</p> 
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/21.jpeg" alt="Floor like texture with dots">
+        <div class="description">
+            <p>Texture 2</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/22.jpeg" alt="Leafs texture">
+        <div class="description">
+            <p>Texture 3</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/23.jpeg" alt="Brick texture small scale">
+        <div class="description">
+            <p>Texture 4</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/24.jpeg" alt="Wood texture">
+        <div class="description">
+            <p>Texture 5</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/25.jpeg" alt="Yellow brick texture big scale">
+        <div class="description">
+            <p>Texture 6</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/26.jpeg" alt="Dots on grey background texture">
+        <div class="description">
+            <p>Texture 7</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/27.jpeg" alt="Dark red clothing like texture">
+        <div class="description">
+            <p>Texture 8</p>
+        </div> 
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/28.JPG" alt="A sunny day in Mexico Cruise port">
+        <div class="description">
+            <p>Gorgeous view from Cruise</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/29.JPG" alt="A long trail of the boat with view of mountains around in a sunny day">
+        <div class="description">
+            <p>Vancouver Island</p>
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/30.JPEG" alt="Mountains around Vancouver">
+        <div class="description">
+            <p>Vancouver Island 2</p> 
+        </div>
+    </div>
+    <div class="gallery-item">
+        <img src="Medias/iPhones-photos/31.JPG" alt="High building in the center of Vancouver">
+        <div class="description">
+            <p>Downtown Vancouver</p> 
+        </div>
+    </div>
+    <div class="gallery-item"> 
+        <img src="Medias/iPhones-photos/32.jpeg" alt="A moutain trail path and epic snow mountain in the background">
+        <div class="description">
+            <p>Wilcox Path</p> 
+        </div>
+    </div>
+</section>
+
 </main>
